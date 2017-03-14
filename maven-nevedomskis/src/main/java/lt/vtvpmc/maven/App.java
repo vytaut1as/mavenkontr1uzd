d28ed07a6877d1b8a7b5e553435bb06ca0ad889e
Vytautas <vytaut1as@gmail.com>
2017-03-14 10:33:08 +0200

    pirmas git commitas

---
+++ maven-nevedomskis/src/main/java/lt/vtvpmc/maven/App.java
@@ -0,0 +1,13 @@
+package lt.vtvpmc.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
